0fd1ff25ad64c0076330168676147c260549ffa8
Warley-Santos <warleywagner95@gmail.com>
2016-11-12 17:55:12 -0200

    Colisão entre meteoros e nave
    + Adicionado evento de colisão entre nave e meteoros;
    + Adicionado som e imagem para o efeito de explosão quando ocorre a
    colisão;
    + Adicionado um favicon;
    - Removido algumas linhas de código inúteis.

--- versao-web/js/game.js
+++ versao-web/js/game.js
@@ -1,6 +1,5 @@
 var pontuacao;
 var velocidadeMovimentacaoMeteoros  = 0.5;
-var setouVelocidade;
 var INCREMENTO_DE_VELOCIDADE = 0.04;
 var nivel;
 var max = 10;
@@ -23,7 +22,7 @@ var Game = {
 				fill: "#ff0044",
 				align: "center"
 			});
-			this.alteraPergunta();
+			this.geraPergunta();
 
 			pontuacao = 0;
 			this.textoPontuacao = this.add.text(this.world.centerX + 325, this.world.centerY - 290, pontuacao,{
@@ -62,10 +61,12 @@ var Game = {
 		this.physics.arcade.overlap(this.tiro, this.meteoroCerto, this.quandoAconteceColisaoCorreta, null, this);
 		this.physics.arcade.overlap(this.tiro, this.meteoroErrado1, this.quandoAconteceColisaoErrada, null, this);
 		this.physics.arcade.overlap(this.tiro, this.meteoroErrado2, this.quandoAconteceColisaoErrada, null, this);
+		this.physics.arcade.overlap(this.navinha, this.meteoroCerto, this.colisaoNaveMeteoroCerto, null, this);
+		this.physics.arcade.overlap(this.navinha, this.meteoroErrado1, this.colisaoNaveMeteoroErrado1, null, this);
+		this.physics.arcade.overlap(this.navinha, this.meteoroErrado2, this.colisaoNaveMeteoroErrado2, null, this);
 
 		this.checkGameOver();
 	},
-
 
 	carregaRecursos : function (){
 
@@ -74,6 +75,7 @@ var Game = {
 		this.load.audio('somRespostaErrada', ['recursos/audio/somRespostaErrada.mp3', 'recursos/audio/somRespostaErrada.ogg']);
 		this.load.audio('somGameOver', ['recursos/audio/somGameOver.mp3', 'recursos/audio/somGameOver.ogg']);
 		this.load.audio('somTema', ['recursos/audio/somTema.mp3', 'recursos/audio/somTema.ogg']);
+		this.load.audio('somExplosao', ['recursos/audio/somExplosao.mp3', 'recursos/audio/somExplosao.ogg']);
 		
 		this.load.image('cenario', 'recursos/imagens/cenario.png');
 		this.load.image('navinha', 'recursos/imagens/navinha.png');
@@ -99,12 +101,18 @@ var Game = {
 		this.somRespostaCerta = this.add.audio('somRespostaCerta');
 		this.somRespostaErrada = this.add.audio('somRespostaErrada');
 		this.somGameOver = this.add.audio('somGameOver');
+		this.somExplosao = this.add.audio('somExplosao');
 	},
 
 	criaNave : function (){
 		this.navinha = this.add.sprite(this.world.centerX, this.world.centerY + 175, 'navinha');
 		this.physics.enable(this.navinha, Phaser.Physics.ARCADE); // aplicar físicas (object, system)
 		this.controles = this.input.keyboard.createCursorKeys(); // retorna um objeto -> up, down, left, right
+	},
+
+	criaExplosao : function(){
+		this.somExplosao.play();
+		this.explosao = this.add.sprite(this.navinha.x, this.navinha.y, 'explosao');
 	},
 
 	criaTiros : function (){
@@ -127,6 +135,12 @@ var Game = {
 	},
 
 	criaMeteoros : function (){
+
+		if(this.colidiu) { //recria a nave junto com os meteoros caso tenha havido uma colisão entre nave e meteoro
+			this.criaNave();
+			this.colidiu = false;
+		}
+		
 		// reseta posição do gurpo no eixo y
 		this.meteoros.y = 0;
 
@@ -163,8 +177,6 @@ var Game = {
 			wordWrapWidth: this.meteoroErrado2.width,
 			align: "center" });
 		this.textErrado2.anchor.set(0.5, 0.5);
-
-
 	},
 
 	quandoAconteceColisaoCorreta : function (tiroQueAcertou, meteoro){
@@ -179,28 +191,9 @@ var Game = {
 		this.textoPontuacao.text = pontuacao;
 		this.somRespostaCerta.play();
 		this.aumentaRangeOperacoes(pontuacao);
-		this.alteraPergunta();
+		this.geraPergunta();
 		this.incrementaVelocidade();
 		this.criaMeteoros();
-
-		// O código abaixo faz com que o grupo de tiros seja destruido e em seguida criado novamente, 
-		//para evitar de acertar meteoros errados
-		/*
-		tiro.removeAll(true, true, true);
-			tiro = jogo.add.group();
-		// Faz com que os objetos do grupo tenham um 'corpo' e em seguida seta o sistema de fisica aplicado a esses corpos
-		tiro.enableBody = true;
-		tiro.physicsBodyType = Phaser.Physics.ARCADE;
-		// Cria um grupo de 30 sprites usando a imagem da key fornecida
-		tiro.createMultiple(30, 'umTiro');
-		// Posiçao do tiro no bico da nave   ---- Altura em que o tiro sai, pra sair da boca da nave e não do meio da tela
-		tiro.setAll('anchor.x', -0.9);
-		tiro.setAll('anchor.y', 0.8);
-		// Faz o objeto ser killado após sair da tela chamando automaticamente a função inWorld que retorna false	
-		tiro.setAll('outOfBoundsKill', true);
-		tiro.setAll('checkWorldBounds', true);
-		*/
-
 	},
 
 	quandoAconteceColisaoErrada : function (tiroQueAcertou, meteoro){
@@ -212,26 +205,49 @@ var Game = {
 		this.textCorreto.kill();
 		this.textErrado1.kill();
 		this.textErrado2.kill();
-		this.alteraPergunta();
+		this.geraPergunta();
 		this.criaMeteoros();
-		// verifica vidas e chama game-over
 		this.vidas--;
 		this.textoVidas.text = this.vidas;
 		this.somRespostaErrada.play();
 		this.checkGameOver();
-		//jogo.state.start('Game-over');
-
 	},
 
+	colisaoNaveMeteoroCerto : function(navinha, meteoro){
+		meteoro.kill();	
+		this.meteoroCerto.kill();
+		this.textCorreto.kill();
+		this.criaExplosao();
+		this.navinha.kill();
+		this.colidiu = true;
+	},
 
-	alteraPergunta : function (){
+	colisaoNaveMeteoroErrado1 : function(navinha, meteoro){
+		meteoro.kill();	
+		this.meteoroErrado1.kill();
+		this.textErrado1.kill();
+		this.criaExplosao();
+		this.navinha.kill();
+		this.colidiu = true;
+	},
+
+	colisaoNaveMeteoroErrado2 : function(navinha, meteoro){
+		meteoro.kill();	
+		this.meteoroErrado2.kill();
+		this.textErrado2.kill();
+		this.criaExplosao();
+		this.navinha.kill();
+		this.colidiu = true;
+	},
+
+	geraPergunta : function (){
 		
 		if(nivel == 1) {
 			var op = 1;
 
 		} else if(nivel == 2) {
 			var op = this.getRandomInt(1, 2);
-		
+
 		} else if(nivel == 3) {
 			var op = this.getRandomInt(1, 3);
 
@@ -279,14 +295,12 @@ var Game = {
 
 	},
 
-	
 	atualizoes : function (){
 
 		this.cenario.tilePosition.y += this.velocidadeScrollCenario;
 		
 		//resetando para 0
 		this.navinha.body.velocity.x = 0;
-
 		
 		if(this.botaoAtirar.isDown){
 			this.atira();	
@@ -354,40 +368,31 @@ var Game = {
 		}
 	},
 
-
 	getRandomInt : function (min, max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	},
 
-	//detecta quando os meteoros não são atingidos
 	checkGameOver : function (){
-		if (this.meteoros.y > 550 && this.vidas > 0) {
+		if (this.meteoros.y > 550 && this.vidas > 0) { //detecta quando os meteoros não são atingidos
 			this.vidas--;
 			this.textoVidas.text = this.vidas;
 			this.meteoroErrado1.kill();
 			this.meteoroErrado2.kill();
 			this.meteoroCerto.kill();
+			this.explosao.kill();
 			this.somRespostaErrada.play();
 			this.criaMeteoros();
-		} else if(this.meteoros.y > 600 && this.vidas <= 0) {
-			//O parametro 0.2 na função .play é o volume do audio
+
+		} else if(this.vidas == 0) {
 			this.somTema.stop();
-			this.somGameOver.play(null, null, 0.2, null, null);
+			this.somGameOver.play(null, null, 0.2, null, null); //O parametro 0.2 na função .play é o volume do audio
 			this.gameOver();
-			max = 10; //seta o valor maximo da range novamente.
-		} else if(this.vidas <= 0){
-			this.somTema.stop();
-			this.somGameOver.play(null, null, 0.2, null, null);
-			this.gameOver();
-			max = 10; //seta o valor maximo da range novamente.
+			max = 10; //seta o valor maximo da range novamente.	
 		}
 	},
 
-	//chama a tela de game over quando acabam as vidas
+	//chama a tela de game over
 	gameOver : function () {
-		if(!setouVelocidade){
-			velocidadeMovimentacaoMeteoros = 0.5;
-		}
 		this.state.start('Game-over');
 	},
 
