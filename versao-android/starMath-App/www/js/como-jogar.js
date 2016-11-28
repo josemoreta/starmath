dd0ed1e2f5be8d9711b3e8af5254b314e613154b
mateusduraes <mduraes1994@gmail.com>
2016-11-28 00:15:52 -0200

    como jogar mobile

--- versao-android/starMath-App/www/js/como-jogar.js
+++ versao-android/starMath-App/www/js/como-jogar.js
@@ -1,15 +1,24 @@
 var ComoJogar = {
 	
 	preload: function(){
-		this.load.image('background', 'recursos/imagens/tela_de_jogo_tuto.jpg');
-		this.load.image('botaoMenu', 'recursos/imagens/botao-menu2.png');
+		this.load.image('background', 'recursos/imagens/background-game-over.png');
+		this.load.image('botaoMenu', 'recursos/imagens/botao-menu.png');
 		
 	},
 
 	create: function(){
-
+		
 		this.add.sprite(0, 0, 'background');
-		this.add.button(this.world.centerX + 180, this.world.centerY + 240, 'botaoMenu', this.voltaProMenu, this);		
+
+		this.textoMeta = this.add.text(50, 50, 'Atire no meteoro certo para ganhar pontos. Utilize os botões na parte inferior da tela ou a inclinação do celular.',{
+			font: '30px Arial',
+			fill: '#ffffff',
+			align: 'center',
+			strokeThickness: 3,
+	    	wordWrap: true,
+	    	wordWrapWidth: 250
+		});
+		this.add.button(this.world.centerX - (245/2), this.world.centerY + 120, 'botaoMenu', this.voltaProMenu, this);		
 	},
 	
 	voltaProMenu: function(){
