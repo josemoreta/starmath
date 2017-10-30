a7cd60625db6ff5b4ccf9d74aaa906df3039b57e
Beatriz Magalhães <mbeatrizmagalhaes@gmail.com>
2017-10-30 22:58:48 +0000

    some fixes

--- versao-web/js/how-to-play.js
+++ versao-web/js/how-to-play.js
@@ -1,13 +1,10 @@
 var HowToPlay = {
-	
 	preload: function(){
 		this.load.image('background', 'resources/images/tela_de_jogo_tuto.jpg');
 		this.load.image('menuButton', 'resources/images/menu-button2.png');
-		
 	},
 
 	create: function(){
-
 		this.add.sprite(0, 0, 'background');
 		this.add.button(this.world.centerX + 180, this.world.centerY + 240, 'menuButton', this.backToMenu, this);		
 	},
@@ -15,5 +12,4 @@ var HowToPlay = {
 	backToMenu: function(){
 		starMath.state.start('Menu');
 	}
-
 };
